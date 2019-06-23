78376143caab81d832ff52ac2c0e3c392506e04f
Nina <ninalouisepedersen@gmail.com>
2019-06-23 12:35:19 +0200

    Commit for new outline of new functions.

---
+++ R/infomax.R
@@ -0,0 +1,21 @@
+#' Infomax implementation
+#'
+#' Implementation of the infomax algorithm to find ICs.
+#'
+#' @param data.mat data matrix where the columns are bands
+#' @param p number of components
+#' @param max maximum number of iterations
+#'
+#' @return ica.arr independent component array
+#' @examples
+#' ica <- infomax(data,3,200)
+#'
+#' @export
+
+fastICAparallel <- function(data.mat, p, max = 200){
+
+  #return(XX <- WW)
+}
+
+
+
